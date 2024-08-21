2cb960a31ce3ffc54a86f1f675e3c797e6917869
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2024-08-21 12:08:49 +0000

    xdd

--- s_t.py
+++ s_t.py
@@ -8,7 +8,7 @@ import pandas_ta as ta
 
 
 # Screener Constants
-fixed_target = 30           # 30 %
+fixed_target = 60           # 30 %
 fixed_stoploss = 20         # 20 %
 number_of_position = 10     # Infinite or fixed
 wallet = 100000             # Wallet Balance
@@ -17,9 +17,9 @@ entry_amount = 10000        # per entry
 increase_percent = 5        # When profit is greater then 10% then only entry amount increased by 5%
 fixed_entry_amount_flag = False
 fixed_target_flag = True
-indicator_exit = True
+indicator_exit = False
 
-file_name = 'V1_n'
+file_name = 'V4_n'
 symbol_list_unfiltered = ns.get_nifty50_with_ns()
 
 
